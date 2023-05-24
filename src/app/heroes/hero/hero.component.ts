5f3b3b42ada2a878cb8eb8fa38ce2f7f4932fd8a
Martín <magadiflo@gmail.com>
2023-05-23 23:07:50 -0500

    One way data binding - enlazado en una sola vía

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -10,4 +10,12 @@ export class HeroComponent {
   public name: string = 'Ironman';
   public age: number = 45;
 
+  public get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
 }
