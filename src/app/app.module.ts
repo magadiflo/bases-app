d3d2cbe812b80b86d94dbd68dabfd145feddac73
Martín <magadiflo@gmail.com>
2023-05-25 22:58:01 -0500

    Módulo DBZ (Dragon Ball Z)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HeroesModule,
     CounterModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
