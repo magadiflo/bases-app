5604731ee6caba82d32a9b0510801bfb26f16e42
Martín <magadiflo@gmail.com>
2023-05-29 23:21:27 -0500

    FormsModule y ngModel

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Character } from '../../interfaces/character.interface';
+
 @Component({
   selector: 'app-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.scss']
 })
 export class AddCharacterComponent {
+
+  public character: Character = { name: '', power: 0 };
+
+  emitCharacter(): void {
+    console.log(this.character);
+  }
 
 }
