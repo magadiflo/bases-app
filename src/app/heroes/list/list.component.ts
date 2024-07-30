e98969afdfb3b37735d550eeada3d061919cf554
Martín <magadiflo@gmail.com>
2024-07-30 13:09:45 -0500

    Implementa componente de lista usando directiva *ngFor y el nuevo flujo de control @for

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,10 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list.component.html',
   styles: ``,
 })
-export class ListComponent {}
+export class ListComponent {
+  public heroes = ['Spiderman', 'Superman'];
+}
